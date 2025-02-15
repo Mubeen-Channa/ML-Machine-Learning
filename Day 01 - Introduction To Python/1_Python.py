2380add9fa73608a200f17e2bc417fc7932e364c
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-02-15 22:03:46 +0500

    Added Day 01 files for Python learning

---
+++ Day 01 - Introduction To Python/1_Python.py
@@ -0,0 +1,34 @@
+# Python is highly recommended for ML, We can also use other programming languages.
+
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+
+# Why Python for ML?
+    # Easy Syntax      → Focus on logic, not complex code. (beginner-friendly)
+    # Rich Libraries   → NumPy, Pandas, Scikit-learn, TensorFlow, PyTorch.
+    # Strong Community → Tons of tutorials, support, and pre-built models.
+
+
+# Alternatives:
+    # R     → Best for statistical analysis.
+    # Java  → Used in enterprise ML applications.
+    # Julia → Faster for numerical computing.
+
+
+# Versatile: Used in web development, data science, AI, automation, and more.
+
+
+# Frist Program
+print("Hello, Mubeen!")     # Output: Hello, Mubeen
+
+
+# Syntax:
+    # Python uses indentation (spaces or tabs) to define code blocks instead of braces {}.
+
+    # ✅ Correct:
+    if True:
+        print("Indented properly")  # 4 spaces (recommended)
+
+    # ❌ Incorrect (IndentationError):
+    if True:
+    print("No indentation")         # Missing indentation
